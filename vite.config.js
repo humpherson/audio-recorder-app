d82e32c834c4328149c675f7e210ea2e6d6fe136
humpherson <dhumpherson@gmail.com>
2024-12-09 18:56:10 +0000

    Vite config and certificates for running localhost on HTTPS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: "./localhost+3-key.pem", // Path to your generated key
+      cert: "./localhost+3.pem", // Path to your generated certificate
+    },
+    host: true,
+    port: 5173,
+  },
+});
